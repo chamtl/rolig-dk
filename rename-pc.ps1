b55c5ff7efc9e4caef63097a4bc50c2b512b0ab9
chamtl <54553542+chamtl@users.noreply.github.com>
2024-09-13 12:57:28 +0200

    Add files via upload

---
+++ rename-pc.ps1
@@ -0,0 +1,70 @@
+﻿# Bail out if we aren't in OOBE
+$TypeDef = @" 
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+  
+namespace Api
+{
+ public class Kernel32
+ {
+   [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+   public static extern int OOBEComplete(ref int bIsOOBEComplete);
+ }
+}
+"@ 
+Add-Type -TypeDefinition $TypeDef -Language CSharp
+   
+# Get device information
+$systemEnclosure = Get-CimInstance -ClassName Win32_SystemEnclosure
+$details = Get-ComputerInfo
+
+if (($null -eq $systemEnclosure.SerialNumber) -or ($systemEnclosure.SerialNumber -eq "")) {
+    
+    $serial = New-Guid
+
+    $serialstring = "$serial"
+
+    #Write-Output $serialstring
+
+    $newserial1 = $serialstring.Replace("-", "")
+
+
+if ($newserial1.Length -gt 8) {
+    $newserial1 = $newserial1.Substring(0, 8)
+}
+
+    $newName = "PC-" + $newserial1 + "-NS"
+
+    #Write-Output $newname
+
+    #Exit
+
+    }
+    
+    else {
+
+    $serial = $systemEnclosure.SerialNumber
+
+    $newName = "PC-" + $serial
+
+    if ($newname.Length -gt 13) {
+        $newname = $newname.Substring(0, 13)
+    }   
+
+    Write-Output $newname
+
+    }
+
+
+# Is the computer name already set?  If so, bail out
+if ($newName -ieq $details.CsName) {
+    Write-Host "No need to rename computer, name is already set to $newName"
+    Exit 0
+}
+ 
+# Set the computer name
+Write-Host "Renaming computer to $($newName)"
+Rename-Computer -NewName $newName -Force
+
